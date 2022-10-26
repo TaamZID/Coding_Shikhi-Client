1fecb1e526f25de70d43a44ece6fcc4e4390c6a1
TaamZID <tamjidalam00@gmail.com>
2022-10-26 16:53:52 +0600

    card-done

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import "./Courses.css";
 import Card from "react-bootstrap/Card";
-import { Button, CardGroup } from "react-bootstrap";
+import { Button, CardGroup, Image } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { MDBBtn } from "mdb-react-ui-kit";
@@ -19,28 +19,35 @@ const Courses = () => {
   return (
     <div>
       <h3>Courses</h3>
-      {courses.map((course) => (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          <div className="col mb-2">
-            <div className="card h-100">
-              <img src={course.image} class="card-img-top" alt="Skyscrapers" />
-              <div className="card-body">
-                <h5 className="card-title">{course.name}</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <MDBBtn className="text-dark" color="light">
-                  <Link to={`category/${course.id}`}>See Details</Link>
-                </MDBBtn>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {courses.map((course) => (
+          <div>
+            <div className="col mb-2">
+              <div className="card h-100">
+                <img
+                  src={course.image}
+                  class="card-img-top"
+                  alt="Skyscrapers"
+                  style={{height:200}}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{course.name}</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <MDBBtn className="text-dark" color="light">
+                    <Link to={`category/${course.id}`}>See Details</Link>
+                  </MDBBtn>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
